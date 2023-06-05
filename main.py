482659ae92c8bc8d4d7da7134d073a9f63720c3a
asasanur <adityasasanur@gmail.com>
2023-06-05 18:12:37 -0400

    detect face bounding box

---
+++ main.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np
+import dlib
+
+cap = cv2.VideoCapture(0)
+detector = dlib.get_frontal_face_detector()
+
+while True:
+    _,frame = cap.read()
+    grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    faces = detector(grayFrame)
+    for face in faces:
+        x1,y1 = face.left(), face.top()
+        x2,y2 = face.right(), face.bottom()
+        cv2.rectangle(frame, (x1,y1),(x2,y2), (255,0,0), 2)
+
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
